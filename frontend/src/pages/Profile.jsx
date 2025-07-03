d9bdd633603d64212b53ffaa28f64861932ed77a
arif-07-09 <157058358+arif-07-09@users.noreply.github.com>
2025-07-03 17:40:56 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API = 'http://localhost:5000';
+  const API = process.env.REACT_APP_API_URL;
 
 const Profile = () => {
   const [user, setUser] = useState(null);
